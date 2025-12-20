89f07857064fbb90850842426c7f98509c51500b
syifaauliyahk <157342891+syifaauliyahk@users.noreply.github.com>
2025-12-20 10:36:16 +0700

    Update epubread.sql

--- epubread.sql
+++ epubread.sql
@@ -1,11 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 20, 2025 at 04:00 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.1.25
+-- 1. Buat Database otomatis jika belum ada
+CREATE DATABASE IF NOT EXISTS epubread;
+USE epubread;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -16,16 +11,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `epubread`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
+-- 2. Buat Tabel-Tabel
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
@@ -35,21 +21,10 @@ CREATE TABLE `users` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `categories`
---
-
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `categories`
--- (DATA PRODI FST)
---
 
 INSERT INTO `categories` (`id`, `name`) VALUES
 (1, 'Agribisnis'),
@@ -60,12 +35,6 @@ INSERT INTO `categories` (`id`, `name`) VALUES
 (6, 'Biologi'),
 (7, 'Kimia'),
 (8, 'Teknik Pertambangan');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `books`
---
 
 CREATE TABLE `books` (
   `id` int(11) NOT NULL,
@@ -80,12 +49,6 @@ CREATE TABLE `books` (
   `category` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `book_chapters`
---
-
 CREATE TABLE `book_chapters` (
   `id` int(11) NOT NULL,
   `book_id` int(11) NOT NULL,
@@ -93,12 +56,6 @@ CREATE TABLE `book_chapters` (
   `chapter_order` int(11) DEFAULT NULL,
   `file_path` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `bookmarks`
---
 
 CREATE TABLE `bookmarks` (
   `id` int(11) NOT NULL,
@@ -110,12 +67,6 @@ CREATE TABLE `bookmarks` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `search_index`
---
-
 CREATE TABLE `search_index` (
   `id` int(11) NOT NULL,
   `book_id` int(11) NOT NULL,
@@ -123,126 +74,68 @@ CREATE TABLE `search_index` (
   `content` longtext DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
 
---
--- Indexes for table `categories`
---
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `books`
---
 ALTER TABLE `books`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`),
   ADD KEY `category_id` (`category_id`);
 
---
--- Indexes for table `book_chapters`
---
 ALTER TABLE `book_chapters`
   ADD PRIMARY KEY (`id`),
   ADD KEY `book_id` (`book_id`);
 
---
--- Indexes for table `bookmarks`
---
 ALTER TABLE `bookmarks`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`),
   ADD KEY `book_id` (`book_id`),
   ADD KEY `chapter_id` (`chapter_index`);
 
---
--- Indexes for table `search_index`
---
 ALTER TABLE `search_index`
   ADD PRIMARY KEY (`id`),
   ADD KEY `book_id` (`book_id`),
   ADD KEY `chapter_id` (`chapter_id`);
+
 ALTER TABLE `search_index` ADD FULLTEXT KEY `idx_content` (`content`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `categories`
---
 ALTER TABLE `categories`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
---
--- AUTO_INCREMENT for table `books`
---
 ALTER TABLE `books`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `book_chapters`
---
 ALTER TABLE `book_chapters`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `bookmarks`
---
 ALTER TABLE `bookmarks`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `search_index`
---
 ALTER TABLE `search_index`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `books`
---
 ALTER TABLE `books`
   ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `books_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE SET NULL;
 
---
--- Constraints for table `book_chapters`
---
 ALTER TABLE `book_chapters`
   ADD CONSTRAINT `book_chapters_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE;
 
---
--- Constraints for table `bookmarks`
---
 ALTER TABLE `bookmarks`
   ADD CONSTRAINT `bookmarks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `bookmarks_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE;
 
---
--- Constraints for table `search_index`
---
 ALTER TABLE `search_index`
   ADD CONSTRAINT `search_index_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `search_index_ibfk_2` FOREIGN KEY (`chapter_id`) REFERENCES `book_chapters` (`id`) ON DELETE CASCADE;
+
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

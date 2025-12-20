b4e688c3b7e3cdca6e8e1d39a9e36cedaa897adf
syifaauliyahk <157342891+syifaauliyahk@users.noreply.github.com>
2025-12-20 09:27:12 +0700

    Update db.php

--- core/db.php
+++ core/db.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/lang_init.php';
 
-$host = "localhost:4306";
+$host = "localhost";
 $user = "root"; // username 
 $pass = "";     // password 
 $dbname = "epubread";
@@ -14,4 +14,5 @@ if ($conn->connect_error) {
     $msg = isset($lang['db_connection_failed']) ? $lang['db_connection_failed'] : "Connection failed: ";
     die($msg . $conn->connect_error);
 }
+
 ?>
